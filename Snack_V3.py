f49a199b8ffbf2dfd80a8b56f4231b4c91159737
TypMitSchnurrbart <61209252+TypMitSchnurrbart@users.noreply.github.com>
2020-02-26 22:07:04 +0100

    Umfassende Änderungen
    Lüppt jetzt; aber noch einige Bugs zu beheben im Speichervorgang (Zeilenumbrüche und Kommas)

--- Snack_V3.py
+++ Snack_V3.py
@@ -5,17 +5,28 @@
 Snack_V3 Opjektorientierter Automat mit Nutzerkonten
 """
 
+import sys
+
 Counter_ID = 0
 Abfrage = True
 User_List = []
 Nutzer_Konten = []
+Produkt_Auswahl = ["Insulin", "Kekse", "Buch"]
+Preise = [4.50, 2.30, 1.69]
+Anz = []
 
 class Produkte:
 
     def __init__(self, Name, Preis, Anzahl):
         self.Name = Name
-        self.Peis = Preis
+        self.Preis = Preis
         self.Anzahl = Anzahl
+
+        Produkt_Auswahl.append(self.Name)
+        print(Produkt_Auswahl)
+
+    def Get_Preis(self):
+        return self.Preis
 
 
 class Nutzer:
@@ -37,6 +48,9 @@ class Nutzer:
     def Get_Bname(self):
         return self.Bname
 
+    def Get_Pw(self):
+        return self.Pw
+
 class Snacc:
 
     def __init__(self):
@@ -50,13 +64,13 @@ class Snacc:
         :return: Kontostand
         """
 
-        Kontostand = Bname.Get_Konto()
+        Kontostand = float(Bname.Get_Konto())
 
         try:
-            Konto_Cache = round(float(input("Wie viel wollen sie einzahlen?")),2)
+            Konto_Cache = round(float(input("Wie viel wollen sie einzahlen? \n")), 2)
 
             if Konto_Cache > 0:
-                Kontostand = Kontostand + Konto_Cache
+                Kontostand = round(float(Kontostand + Konto_Cache), 2)
                 Bname.Set_Konto(Kontostand)
             else:
                 print("Frech Kati.")
@@ -64,33 +78,47 @@ class Snacc:
         except ValueError:
             print("Geben sie nur gültige Werte ein! \n")
 
-    def Kaufen(self, Bname):
+
+    def Kaufen(self, Bname, Prod, Anzahl):
         """
         Kaufen der Produkte
-        :param Bname zur Addressierung des zu belastenden Kontos
-        :return: Kontostand und Produkt-Vorrat
+        :param Bname:
+        :param Prod:
+        :param Anzahl:
+        :return:
         """
+        Kontostand = Bname.Get_Konto()
+
+        for Pro in range(0, len(Prod)):
+            print("{0}  Preis: {1}€  Rest: {2}   [{3}]" .format(Prod[Pro], Preise[Pro], Anzahl[Pro], Pro ))
+
+        try:
+            Auswahl = int(input("""Was wollen sie kaufen? \n"""))
+        except ValueError:
+            sys.exit()
+
+        if Kontostand >= Preise[Auswahl]:
+            if Anz[Auswahl] > 0:
+
+                Anz[Auswahl] = Anz[Auswahl] - 1
+                Kontostand = round(Kontostand - Preise[Auswahl], 2)
+
+                print("Sie haben {0} gekauft \n Restguthaben: {1}€ \n"
+                      .format(Prod[Auswahl], Kontostand))
+                Bname.Set_Konto(Kontostand)
+
+            else:
+                print("Leider nicht mehr vorrätig! \n")
+
+        else:
+            print("Sie haben nicht genug Geld!\n")
 
 
-def Nutzer_Erstellung():
+
+
+def Produkt_Menge():
     """
-    Einrichten der Nuter in ihre Klasse
-    """
-    Save = []
-
-    Read = open("Snack_Speicher.txt", "r")
-    for Lines in Read:
-        Save.append(Lines)
-
-    Save = Save[0].split("\n")
-    Save = Save[0].split(", ")
-    
-    for Counter in range(0,len(Save),3):
-        Save[Counter] = Nutzer(Save[Counter], Save[Counter+1], Save[Counter+2])
-
-def Produkt_Erstellung():
-    """
-    Erstellen der Produkte
+    Erstellen der Produkte Mengen
     :return:
     """
     Save = []
@@ -100,10 +128,69 @@ def Produkt_Erstellung():
         Save.append(Lines)
 
     Save = Save[1].split(", ")
-    print(Save)
 
-    for Counter in range(0, len(Save), 3):
-        Save[Counter] = Produkte(Save[Counter], Save[Counter + 1], Save[Counter + 2])
+    for Count in range(0,len(Save)):
+        Save[Count] = int(Save[Count])
+
+    print(Save)
+    return Save
+
+
+
+
+def Speichern(Benutzername, Comt, Anzahlen):
+    """
+    Speichern der Benutzer und Mengen
+    :param Benutzername:
+    :param Comt:
+    :param Anzahlen:
+    :return:
+    """
+    Save = []
+
+    Balance = Benutzername.Get_Konto()
+
+    Leser = open("Snack_Speicher.txt", "r")
+
+    for Lines in Leser:
+        Save.append(Lines)
+
+    Leser.close()
+
+    Save = Save[0].split(", ")
+
+    Leser = open("Snack_Speicher.txt", "w")
+
+    if Comt >= 0:
+        Save[Comt + 2] = Balance
+
+        for i in Save:
+            End = len(Save) - 1
+            if i == Save[End]:
+                Leser.write("{0}" .format(i))
+            else:
+                Leser.write("{0}, " .format(i))
+
+
+    else:
+        Save.append(Benutzername.Get_Bname())
+        Save.append(Benutzername.Get_Pw())
+        Save.append(Benutzername.Get_Konto())
+
+
+    Save = Anzahlen
+
+    for i in Save:
+        End = len(Save) - 1
+        if i == Save[End]:
+            Leser.write("{0}" .format(i))
+        else:
+            Leser.write("{0}, " .format(i))
+
+    Leser.close()
+    sys.exit(201)
+
+
 
     
 
@@ -136,7 +223,7 @@ if __name__ == "__main__":
 
                     Konto_Value = User_List[Com+2]
 
-                    Bname_In = Nutzer(Bname_In, Pw_Cache, Konto_Value)
+                    Bname_In = Nutzer(Bname_In, Pw_Cache, round(float(Konto_Value),2))
 
                     User_List.pop(Com+2)
                     User_List.pop(Com+1)
@@ -144,6 +231,10 @@ if __name__ == "__main__":
 
                     print(User_List)
                     break
+
+                else:
+                    print("Falsches Passwort!")
+                    exit()
 
             elif Name_Cache != Bname_In and Com == len(User_List)+1:
                 print("Kein gültiger Benutzername!")
@@ -171,6 +262,7 @@ if __name__ == "__main__":
 
                 elif Name_Cache != Bname_In and Com == len(User_List)-1:
                     Bname_In = Nutzer(Bname_In, Pw_In)
+                    Com = -1
                     Abfrage = False
                     print("Ihr Konto wurde erstellt!")
 
@@ -182,28 +274,31 @@ if __name__ == "__main__":
     Automat = Snacc()
 
     #Erstellung der Nutzer
-    Nutzer_Erstellung()
+    #Nutzer_Erstellung()
 
     #Erstellung der Produkte
-    Produkt_Erstellung()
+    Anz = Produkt_Menge()
 
     while True:
 
         Input_Cache = input("""\nWillkommen beim Snacc! \n Eingeloggt als: {0}
+        Guthaben: {1}€
         Was wollen sie tun?
             Einzahlen   [1]
             Kaufen      [2]
-            Beenden     [3]\n""" .format(Bname_In.Get_Bname()))
+            Beenden     [3]\n""" .format(Bname_In.Get_Bname(), Bname_In.Get_Konto()))
 
 
 
         if Input_Cache == "1":
-            print(Bname_In.Get_Konto())  #Maybe Dictonary to convert Str into "Code"
             Automat.Einzahlung(Bname_In)
-            print(Bname_In.Get_Konto())
+            print("Ihr neuer Kontostand: {0}€ \n" .format(Bname_In.Get_Konto()))
 
         elif Input_Cache == "2":
-            Automat.Kaufen(Bname_In)
+            Automat.Kaufen(Bname_In, Produkt_Auswahl, Anz)
+
+        elif Input_Cache == "3":
+            Speichern(Bname_In, Counter_ID, Anz)
 
 
 
